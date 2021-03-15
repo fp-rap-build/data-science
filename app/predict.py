b40921ec2f0078126738926d9776957dddf58de3
iesous-kurios <j.wylie.81@gmail.com>
2021-03-14 22:29:12 -0700

    requirements.txt

--- app/predict.py
+++ app/predict.py
@@ -10,6 +10,45 @@ from pydantic import BaseModel, Field, validator
 log = logging.getLogger(__name__)
 router = APIRouter()
 
+user = pd.read_csv('app/spokane_zipcodes.csv', header='infer')
+
+income_filter = {1:43350, 2:49550, 3:55750,
+                  4:61900, 5:66900, 6:71580,
+                  7:76800, 8:81750}
+zips_filter = [99001, 99001, 99001, 99001, 99001, 99001, 99001, 99001, 99003,
+        99003, 99003, 99003, 99003, 99003, 99003, 99003, 99004, 99004,
+        99004, 99004, 99004, 99004, 99004, 99004, 99004, 99004, 99005,
+        99005, 99005, 99005, 99005, 99005, 99006, 99006, 99006, 99006,
+        99006, 99006, 99006, 99006, 99009, 99009, 99009, 99009, 99009,
+        99009, 99009, 99009, 99011, 99011, 99011, 99011, 99011, 99011,
+        99011, 99011, 99012, 99012, 99012, 99012, 99012, 99012, 99012,
+        99012, 99016, 99016, 99016, 99016, 99016, 99016, 99016, 99016,
+        99018, 99018, 99018, 99018, 99018, 99018, 99018, 99018, 99019,
+        99019, 99019, 99019, 99019, 99019, 99019, 99019, 99020, 99020,
+        99020, 99020, 99020, 99020, 99020, 99020, 99021, 99021, 99021,
+        99021, 99021, 99021, 99021, 99021, 99022, 99022, 99022, 99022,
+        99022, 99022, 99022, 99022, 99023, 99023, 99023, 99023, 99023,
+        99023, 99023, 99023, 99025, 99025, 99025, 99025, 99025, 99025,
+        99025, 99025, 99027, 99027, 99027, 99027, 99027, 99027, 99027,
+        99027, 99030, 99030, 99030, 99030, 99030, 99030, 99030, 99030,
+        99031, 99031, 99031, 99031, 99031, 99031, 99031, 99031, 99036,
+        99036, 99036, 99036, 99036, 99036, 99036, 99036, 99037, 99037,
+        99037, 99037, 99037, 99037, 99037, 99037, 99039, 99039, 99039,
+        99039, 99039, 99039, 99039, 99039, 99201, 99201, 99201, 99201,
+        99201, 99201, 99201, 99201, 99202, 99202, 99202, 99202, 99202,
+        99202, 99202, 99202, 99203, 99203, 99203, 99203, 99203, 99203,
+        99203, 99203, 99204, 99204, 99204, 99204, 99204, 99204, 99204,
+        99204, 99205, 99205, 99205, 99205, 99205, 99205, 99205, 99205,
+        99206, 99206, 99206, 99206, 99206, 99206, 99206, 99206, 99207,
+        99207, 99207, 99207, 99207, 99207, 99207, 99207, 99208, 99208,
+        99208, 99208, 99208, 99208, 99208, 99208, 99212, 99212, 99212,
+        99212, 99212, 99212, 99212, 99212, 99216, 99216, 99216, 99216,
+        99216, 99216, 99216, 99216, 99217, 99217, 99217, 99217, 99217,
+        99217, 99217, 99217, 99218, 99218, 99218, 99218, 99218, 99218,
+        99218, 99218, 99223, 99223, 99223, 99223, 99223, 99223, 99223,
+        99223, 99224, 99224, 99224, 99224, 99224, 99224, 99224, 99224,
+        99016, 99016, 99016, 99016, 99016, 99016, 99016, 99016]
+
 class Item(BaseModel):
     """Use this data model to parse the request body JSON."""
 
@@ -23,45 +62,45 @@ class Item(BaseModel):
 @router.post('/predict')
 async def determine_eligibility(zipcode, family_size, income, unEmp90, foodWrkr):
 
-    user = pd.read_csv('app/spokane_zipcodes.csv', header='infer')
+    
     
     try:
-        allowed_zips = set(user['zipcode'])
-        user_income = income * 12
-        
-        if zipcode in allowed_zips:
+        print(income_filter[family_size])
+        user_income = int(income * 12)
+        print(f'user income : {user_income}')
+        for z in user['zipcode']:
+            if int(z) == zipcode:
+            
+            
+                if user_income <= income_filter[family_size]:
+
+                    print(f'userincome if{user_income}')
+
 
             
 
-            income_goal = user[(user['zipcode'] == zipcode) & (user['family_members'] == family_size)]
-            print('Found it!')
-            print(f'user income: {user_income}')
-            income_goal = income_goal['annual_income']
-            print(income_goal)
-            if (int(user_income) <= int(income_goal)):
-                print('made it inside user income statement')
+                    if unEmp90 == True:
 
-                if unEmp90 == True:
-                    return {
-                        'SNAP': 1,
-                        'CC': 0,
-                        'FP': 0
-                }
+                        print('made it past unemp!')
+                        return {
+                            'SNAP': 1,
+                            'CC': 0,
+                            'FP': 0
+                            }
 
+                    else:
+                        return {
+                            'SNAP' : 0,
+                            'CC' : 0,
+                            'FP' : 1
+                        }
+
+            
                 else:
                     return {
                         'SNAP': 0,
                         'CC': 0,
                         'FP': 1
-
                     }
-
-            
-            else:
-                return {
-                    'SNAP': 0,
-                    'CC': 0,
-                    'FP': 1
-                }
     except:
-        return f"Sorry, we don't offer services in {zip} yet, please contact yo momma"
+        return f"Sorry, we don't offer services in {zipcode} yet, please contact yo momma"
